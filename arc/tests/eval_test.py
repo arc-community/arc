bfbcefb17825364bdb47a7aaea9d40a3afdffcd8
Yannic Kilcher <yk@users.noreply.github.com>
2022-06-25 20:19:48 +0200

    added agent test

--- arc/tests/eval_test.py
+++ arc/tests/eval_test.py
@@ -5,6 +5,8 @@ from pathlib import Path
 import pytest
 
 import arc.eval
+from arc.agents import Agent
+from arc.interface import Board, BoardData, Riddle
 from arc.settings import settings
 from arc.utils import dataset
 
@@ -31,4 +33,19 @@ def test_eval_solution_for_riddle(riddle1):
     eval_result = arc.eval.eval_solution_for_riddle(
         riddle1, [riddle1.test[0].output.data]
     )  # cheating
+    assert eval_result.score == 1.0
+
+
+class CheatingAgent(Agent):
+    def solve_test_sample(self, riddle: Riddle, test: Board) -> BoardData:
+        for t in riddle.test:
+            if t.input == test:
+                return t.output.data
+        else:
+            assert False
+
+
+def test_evaluate_agent_on_riddle(riddle1):
+    agent = CheatingAgent()
+    eval_result = arc.eval.evaluate_agent_on_riddle(agent, riddle1)
     assert eval_result.score == 1.0
